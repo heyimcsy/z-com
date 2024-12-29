572500ef690b8fc21eeda6930cc4ab890c7a11e3
sychoi <cinnamon1995@naver.com>
2024-12-30 01:05:23 +0900

    login 페이지 stack 오류 해결

--- src/app/(beforeLogin)/i/flow/login/page.tsx
+++ src/app/(beforeLogin)/i/flow/login/page.tsx
@@ -1,10 +1,7 @@
-export default function LoginModal() {
+import LoginModal from "@/app/(beforeLogin)/_component/LoginModal";
 
+export default function Login() {
     return (
         <LoginModal />
     );
 }
-
-
-// 서버 컴포넌트 next.js 서버에서 돈다. 비동기로 컴포넌트를 돌릴수도 있다 서버컴포넌트는 데이터를 다룰 때 좋다.
-// 클라이언트 컴포넌트
