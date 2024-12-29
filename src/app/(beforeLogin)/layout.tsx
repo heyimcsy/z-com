d3811aedede658c387ef2ac6548032af279eabb6
sychoi <cinnamon1995@naver.com>
2024-12-29 23:45:36 +0900

    default 페이지 생성

--- src/app/(beforeLogin)/layout.tsx
+++ src/app/(beforeLogin)/layout.tsx
@@ -13,3 +13,6 @@ export default function Layout({children, modal}:Props){
         </div>
     )
 }
+
+//주소가 localhose:3001일 때는 children --> page.tsx. modal -> @modal/default.tsx
+//주소가 localhost:3001/i/flow/login 일 때는 children -> i/flow/login/page.tsx, modal-> @/modal/i/flow/login/page.tsx
